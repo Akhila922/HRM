6c12d1490403329608e9f03f58b398c272754ae2
Bondada-Vighnesh <bondada.vighnesh@capgemini.com>
2025-11-07 14:30:33 +0530

    Delete src/main directory

--- src/main/java/com/cg/sprint/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.sprint.repository;
-
-
-import com.cg.sprint.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, String> {
-    // No extra methods needed for now
-}
--- src/main/java/com/cg/sprint/service/JobHistoryService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.sprint.service;
-
-import com.cg.sprint.dto.JobHistoryDTO;
-import java.util.List;
-
-public interface JobHistoryService {
-    List<JobHistoryDTO> getAllJobHistories();
-    JobHistoryDTO getJobHistory(Long employeeId, String startDate);
-}
