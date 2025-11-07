1fea5b1154ddc488831c2bad4540b6a76fb46a6b
swathi16136 <CHENNUBOYINA-RAMA.SWATHI@CAPGEMINI.COM>
2025-11-07 17:22:27 +0530

    Update LocationRepositorySwathi.java

--- src/main/java/com/cg/hrms/repositories/LocationRepositorySwathi.java
+++ src/main/java/com/cg/hrms/repositories/LocationRepositorySwathi.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepositorySwathi extends JpaRepository<Location, Long> {
+    
     Location findByCityIgnoreCase(String city);
+
 }
